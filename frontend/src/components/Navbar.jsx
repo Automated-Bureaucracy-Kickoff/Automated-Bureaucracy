22465ada255c76e9afbca412e664bac9d092499d
Lakshay Jain <lakshaybusiness.12@gmail.com>
2025-02-09 20:27:47 +0530

    Fixed nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -20,49 +20,20 @@ const Navbar = () => {
         <div className="text-xl font-bold">
           <Link to="/">
             <div className="flex items-center space-x-2">
-              <h1>Automated Bureaucracy</h1>
+              <h1 className="md:text-3xl">Automated Bureaucracy</h1>
             </div>
           </Link>
         </div>
 
-        {/* Hamburger Icon (Visible on small screens) */}
-        <button
-          className="md:hidden block text-black dark:text-white focus:outline-none"
-          onClick={toggleMenu}
-          aria-label="Toggle Menu"
-        >
-          {isMenuOpen ? (
-            <Close fontSize="large" />
-          ) : (
-            <Menu fontSize="large" />
-          )}
-        </button>
 
         {/* Links for Desktop */}
-        <ul className="hidden md:flex items-center space-x-6">
+        <ul className="flex items-center space-x-6">
             <li>
                 <DarkToggle></DarkToggle>
             </li>
             
         </ul>
 
-        {/* Mobile Menu (Visible when menu icon is clicked) */}
-        {isMenuOpen && (
-          <ul className="md:hidden sticky top-0 w-full bg-zinc-200 dark:bg-zinc-800 flex flex-col space-y-4 p-4 shadow-lg">
-         
-                <li>
-                    <Link 
-                        to="/account" 
-                        className="text-gray-700 dark:text-gray-200 hover:text-gray-500 dark:hover:text-gray-400"
-                    >
-                        <AccountBoxIcon fontSize="large" />
-                    </Link>
-                </li>
-                <li>
-                    <DarkToggle></DarkToggle>
-                </li>
-          </ul>
-        )}
       </div>
     </nav>
   );
