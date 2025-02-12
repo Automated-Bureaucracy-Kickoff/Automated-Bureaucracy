127dcfad1ae3cdc5d3e0a3a940e44735ccad50bd
Blake DeHaas <realblakedehaas@gmail.com>
2025-02-11 21:47:57 -0700

    Fixed scrollToTop when changing route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import { store } from './redux/store/store.js'
 import Navbar from './components/Navbar.jsx'
 import MainPage from './pages/MainPage.jsx'
@@ -8,20 +8,23 @@ import LandingPage from './pages/HomePage.jsx'
 import LandingNavbar from './components/landingPageNavbar.jsx'
 import { useEffect } from 'react'
 import AboutUsPage from './pages/AboutUsPage'
+import useScrollToTop from './hooks/useScrollToTop'
+
+function ScrollToTop() {
+  useScrollToTop();
+  return null;
+}
 
 function App() {
-
     return (
         <BrowserRouter>
             <Provider store={store}>
-
+                <ScrollToTop />
                 <Routes>
                     {/* <Route path='/' element={<> <HomePage/> </>}/> */}
                     <Route path='/' element={<>
                         <LandingNavbar />
-
                         <LandingPage />
-
                     </>} />
                     <Route path='/main' element={
                         <div className="overflowremover flex flex-col w-screen h-screen  max-h-full  bg-zinc-100 dark:bg-zinc-900 dark:text-zinc-100">
@@ -30,17 +33,14 @@ function App() {
                                 <MainPage />
                             </div>
                         </div>
-
                     } />
                     <Route path='/aboutus' element={<>
                         <LandingNavbar />
                         <AboutUsPage />
                     </>} />
                 </Routes>
-
             </Provider>
         </BrowserRouter>
-
     )
 }
 
